01e97e59d2316056afaa9e856ab1fc9c66ce5b70
Scott M. Litvinoff <scott@Scotts-MBP-14.local>
2024-04-01 23:02:34 -0400

    Added Bonfida name resolution

--- read-data/check-balance.ts
+++ read-data/check-balance.ts
@@ -1,21 +1,12 @@
 import { Connection, clusterApiUrl, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
+import {getHashedName, getNameAccountKey, NameRegistryState } from "@bonfida/spl-name-service";
+
 // Parse the command line parameters
 const suppliedPublicKey = process.argv[2];
 if (!suppliedPublicKey) 
 {
   console.log("Please supply a public key to check the balance of");
-  process.exit(-1);
-}
-
-let publicKey = null;
-try 
-{
-  publicKey = new PublicKey(suppliedPublicKey);
-}
-catch (e)
-{
-  console.log("Invalid public key");
   process.exit(-1);
 }
 
@@ -36,6 +27,36 @@ if(netToConnect)
 
 // Connect to network
 const connection = new Connection(apiUrl, "confirmed");
+
+// Check if the supplied key is a ".sol" domain that needs to be resolved
+let resolvedPublicKey = suppliedPublicKey;
+if(suppliedPublicKey.search(new RegExp("\.sol$", "gi")) != -1)
+{
+  const hashedName = await getHashedName(resolvedPublicKey.replace(".sol", ""));
+  const nameAccountKey = await getNameAccountKey(
+    hashedName,
+    undefined,
+    new PublicKey("58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx") // SOL TLD Authority
+  );
+  const owner = await NameRegistryState.retrieve(
+    connection,
+    nameAccountKey
+  );
+  
+  resolvedPublicKey = owner.registry.owner;
+  console.log(`Resolved public key: ${resolvedPublicKey}`);
+}
+
+let publicKey = null;
+try 
+{
+  publicKey = new PublicKey(resolvedPublicKey);
+}
+catch (e)
+{
+  console.log("Invalid public key");
+  process.exit(-1);
+}
 
 const balanceInLamports = await connection.getBalance(publicKey);
 const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
